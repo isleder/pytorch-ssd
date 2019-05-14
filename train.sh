de36547038205a73e15c8fe058fcc85e1bf3edee
Istvan Sleder <istvan@sleder.com>
2019-05-14 11:38:43 -0400

    train sh

---
+++ train.sh
@@ -0,0 +1,1 @@
+python3 train_ssd.py --dataset_type voc  --datasets ~/data/VOC0712/VOC2007 ~/data/VOC0712/VOC2012 --validation_dataset ~/data/VOC0712/test/VOC2007/ --net mb2-ssd-lite --base_net models/mb2-imagenet-71_8.pth  --scheduler cosine --lr 0.01 --t_max 200 --validation_epochs 5 --num_epochs 20
